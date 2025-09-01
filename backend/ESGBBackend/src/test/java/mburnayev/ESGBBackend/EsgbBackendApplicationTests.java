9e919082eed355615136521717c1cd44f1846bbe
mburnayev <misha@burnayev.com>
2025-09-01 12:17:34 -0400

    backend template added

---
+++ backend/ESGBBackend/src/test/java/mburnayev/ESGBBackend/EsgbBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package mburnayev.ESGBBackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EsgbBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
